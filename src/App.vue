05d935ffef01080538f390c2f4d4d572164d2d18
TimiBolaji <mattybolaji@gmail.com>
2023-12-30 12:31:06 +0100

    validate note

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
+<script setup>
+  import {ref} from "vue";
+
+  const showModal = ref(false)
+  const newNote = ref("")
+  const errorMessage = ref("")
+  const notes = ref([])
+
+  function getRandomColor(){
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+  
+  }
+  
+  const addNote = () =>{
+     if(newNote.value.length < 10){
+       return errorMessage.value = "Note needs to be 10 characters or more.."
+     }
+    notes.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor()
+    })
+    showModal.value = false
+    newNote.value =""
+  }
+ 
+</script>
+
 <template>
   <main>
-
-    <div class="overlay">
+    <div v-if="showModal" class="overlay">
 
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10"></textarea>
+        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
 
-        <button>Add Note</button>
-        <button class="close">Close</button>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote">Add Note</button>
+        <button class="close" @click="showModal = false">Close</button>
       </div>
     </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>+</button>
-      </header>
+        <button @click="showModal = true">+</button>
+      </header> 
       <div class="cards-container">
-        <div class="card">
-          <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptate consectetur libero ducimus repellat, quibusdam, tempora pariatur, illo sed quam harum commodi rerum non eos alias culpa iure saepe quas?</p>
-          <p class="date">04/12/2023</p>
+        <div v-for="note in notes" 
+        :key="notes.id"
+        class="card" :style="{backgroundColor: note.backgroundColor}">
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
-        <div class="card">
-          <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptate consectetur libero ducimus repellat, quibusdam, tempora pariatur, illo sed quam harum commodi rerum non eos alias culpa iure saepe quas?</p>
-          <p class="date">04/12/2023</p>
-        </div>
-         
+
       </div>
     </div> 
   </main>
@@ -78,7 +105,7 @@ header button{
    
 
 }
-.date{
+.card .date{
   font-weight: bold;
   font-size: 12.5px;
 }
@@ -120,5 +147,8 @@ header button{
 .modal .close{ 
   background-color: red;
   margin-top: 7px;
+}
+.modal p{
+  color: red;
 }
 </style>
