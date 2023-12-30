c253d72dbbb6033877d950b8d581ec97bf66b067
TimiBolaji <mattybolaji@gmail.com>
2023-12-30 02:24:51 +0100

    Added and Styled Note Modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
 
+    <div class="overlay">
+
+      <div class="modal">
+        <textarea name="note" id="note" cols="30" rows="10"></textarea>
+
+        <button>Add Note</button>
+        <button class="close">Close</button>
+      </div>
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
@@ -44,7 +53,7 @@ h1{
   margin-bottom: 25px;
   font-size: 75px;
 }
-button{
+header button{
    border: none;
    padding: 10px;
    width: 50px;
@@ -77,5 +86,39 @@ button{
   display: flex;
   flex-wrap: wrap;
 
+}
+.overlay{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal{
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+.modal button{
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: burlywood ;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px ;
+}
+.modal .close{ 
+  background-color: red;
+  margin-top: 7px;
 }
 </style>
